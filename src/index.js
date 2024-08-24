a4ba951e335ce5504710217d6c33328b9a3b1db3
Siil777 <pavel.ivanov.thk@gmail.com>
2024-08-24 16:15:19 +0300

    feat: morse decrypted

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    //split input on ten chukns
+    return expr.match(/.{10}/g)
+               .map(arendaja=>{
+                if(arendaja==='**********'){
+                    return ' '; //if input 10* it is a space
+                }else{
+                    const decode = arendaja.match(/.{2}/g) //find certain pairs
+                    .map(spaceships=>{
+                        if(spaceships==='10') return '.'; //if input = 10 this is dot
+                        if(spaceships==='11') return '-'; //if input = 11 this is dash
+                        return ''; //zero or empty string
+                    })
+                    .join('')//join back string
+                    return MORSE_TABLE[decode] || ' ';
+                }
+               })
+               .join('')
 }
 
 module.exports = {
